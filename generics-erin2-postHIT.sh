b3008f5d4248e2c37c3176a40286ffc5133f91bc
erin <festethejester@gmail.com>
2014-06-17 09:56:34 -0700

    remove all old files

--- generics-erin2-postHIT.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-cd /home/feste/opt/aws-mturk-clt-1.3.1/bin
-./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /home/feste/CoCoLab/generics/generics-erin2 -input /home/feste/CoCoLab/generics/generics-erin2.input -question /home/feste/CoCoLab/generics/generics-erin2.question -properties /home/feste/CoCoLab/generics/generics-erin2.properties -maxhits 1
